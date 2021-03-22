1d2de2bd11a58e47f83221118922fc9a5fc259dd
bartosz0 <methkill@gmail.com>
2021-03-22 21:12:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NewStopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type 'start' to start stopwatch.");
+            Console.WriteLine("Please type 'stop' to stop stopwatch.");
+            Console.WriteLine("Please type 'exit' to exit.");
+
+
+            string command = "";
+
+            do
+            {
+                try
+                {
+                    command = Console.ReadLine().ToLower();
+
+                    if (command == "start")
+                    {
+                        Console.Clear();
+                        Stopwatch.Start();
+                        Console.WriteLine("Stopwatch running...\nType 'stop' to calculate the time or 'exit' to leave.");
+                    }
+                    else if (command == "stop")
+                    {
+                        Console.Clear();
+                        var elapsedTime = Stopwatch.Stop();
+                        Console.WriteLine($"Elapsed time = {elapsedTime}");
+                        Console.WriteLine("Type 'start' to start new stopwatch or 'exit' to leave.");
+                    }
+                    else if (command == "exit")
+                    {
+                        Console.WriteLine("Thank you for using my stopwatch program!");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid input!\n");
+                        Console.WriteLine("Please type 'start' to start stopwatch.");
+                        Console.WriteLine("Please type 'stop' to stop stopwatch.");
+                        Console.WriteLine("Please type 'exit' to exit.");
+                        continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error details: {0}", ex.Message);
+                }
+
+            } while (command != "exit");
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NewStopwatch
+{
+    class Stopwatch
+    {
+        private static bool _timerOn = false;
+        private static DateTime _startTime;
+        private static DateTime _stopTime;
+
+        public static void Start()
+        {
+            if (_timerOn == true)
+            {
+                throw new InvalidOperationException("Stopwatch is already running - type 'stop'.");
+            }
+            _timerOn = true;
+            _startTime = DateTime.Now;
+        }
+
+        public static TimeSpan Stop()
+        {
+            if (_timerOn == false)
+            {
+                throw new InvalidOperationException("Stopwatch is not yet running - type 'start'.");
+            }
+            _timerOn = false;
+            _stopTime = DateTime.Now;
+
+            return _stopTime - _startTime;
+        }
+    }
+}
